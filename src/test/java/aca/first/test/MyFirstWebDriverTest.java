5ccf61ef957d56d891d3ffb7c26ce8ca01b40666
sonash79 <sona_sh@inbox.ru>
2017-04-07 22:43:58 +0400

    verifyLoginFields() test

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,16 +1,36 @@
 package aca.first.test;
-
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class MyFirstWebDriverTest {
+	@Test(enabled=false)
+	public void myFirstTest(){
+		 System.setProperty("webdriver.chrome.driver", "/Users/sonash79/Downloads/chromedriver");
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("http://aca.am");
+		 driver.close();
+		 driver.quit();
+	}
 	
 	@Test()
-	public void myFirstTest(){
-		 WebDriver driver = new FirefoxDriver();
-		 driver.get("http://aca.am");
+	public void verifyLoginFields(){
+		 System.setProperty("webdriver.chrome.driver", "/Users/sonash79/Downloads/chromedriver");
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.facebook.com");
+		 int elementIsPresent = driver.findElements(By.id("email")).size();
+		 if (elementIsPresent>0){
+			 System.out.println("'Email or Phone' field is present");
+		 }
+		 
+		 elementIsPresent = driver.findElements(By.id("pass")).size();
+		 if (elementIsPresent>0){
+			 System.out.println("'Password' field is present");
+		 }
+		 
 		 driver.close();
 		 driver.quit();
 	}
